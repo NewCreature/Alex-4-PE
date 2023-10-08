5f2c934eee28753033e81112a63c4c70f601be98
Todd Cope <todd@t3-i.com>
2023-10-08 14:55:11 -0500

    Allow FPS to be adjusted with the 'graphics->fps' setting in the config file.

--- src/timer.c
+++ src/timer.c
@@ -60,7 +60,7 @@ int install_timers() {
 	LOCK_FUNCTION(cycle_counter);
 	install_int(cycle_counter, 20);
 	#ifdef ALLEGRO_LEGACY
-		all_adjust_int_speed(cycle_counter, 1.0 / 50.0);
+		all_adjust_int_speed(cycle_counter, 1.0 / (float)get_config_int("graphics", "fps", 50));
 	#endif
 
 	game_count ++;
